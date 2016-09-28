de3e736732f41c0ef8246c74e18949577fa225d6
Shaun Howard <shaun.howard@genomoncology.com>
2016-09-27 23:33:08 -0400

    class progress

--- project2.m
+++ project2.m
@@ -7,22 +7,45 @@ reference = imread('pics/cantilever_layout1.bmp');
 %figure, imshow(reference)
  
 %invoke MATLAB two image selection tool
-[movingPoints,fixedPoints] = cpselect(unregistered(:,:,1),reference, 'Wait', true);
+%[movingPoints,fixedPoints] = cpselect(unregistered(:,:,1),reference, 'Wait', true);
 
-% create affine transform from unregistered image to reference image
-tform = fitgeotrans(movingPoints,fixedPoints,'affine');
+ptsOriginal  = detectSURFFeatures(reference);
+ptsDistorted = detectSURFFeatures(unregistered);
+[featuresOriginal,validPtsOriginal] = extractFeatures(reference,ptsOriginal);
+[featuresDistorted,validPtsDistorted] = extractFeatures(unregistered,ptsDistorted);
 
-% apply affine transform on unregistered image to warp image
-warped = imwarp(unregistered, tform, 'OutputView', imref2d(size(reference)));
-
-[optimizer,metric] = imregconfig('Multimodal');
-moving = rgb2gray(warped);
-fixed = rgb2gray(reference);
-% attempt to register warper image with reference image using multimodal
-% registration optimization and similarity metric
-registered = imregister(moving, fixed, 'Similarity', optimizer, metric);
+index_pairs = matchFeatures(featuresOriginal,featuresDistorted);
+matchedPtsOriginal  = validPtsOriginal(index_pairs(:,1));
+matchedPtsDistorted = validPtsDistorted(index_pairs(:,2));
 figure;
-imshowpair(registered,reference);
+showMatchedFeatures(original,unregistered,matchedPtsOriginal,matchedPtsDistorted);
+title('Matched SURF points,including outliers');
+
+[tform,inlierPtsDistorted,inlierPtsOriginal] = ...
+    estimateGeometricTransform(matchedPtsDistorted,matchedPtsOriginal,...
+    'similarity');
+figure;
+
+showMatchedFeatures(reference,unregistered,...
+    inlierPtsOriginal,inlierPtsDistorted);
+title('Matched inlier points');
+
+% % create affine transform from unregistered image to reference image
+% tform = fitgeotrans(movingPoints,fixedPoints,'projective');
+% 
+% % apply affine transform on unregistered image to warp image
+% warped = imwarp(unregistered, tform, 'OutputView', imref2d(size(reference)));
+% 
+% % compute MSE between the two
+% [optimizer,metric] = imregconfig('multimodal');
+% moving = rgb2gray(warped);
+% fixed = rgb2gray(reference);
+% 
+% % attempt to register warper image with reference image using multimodal
+% % registration optimization and similarity metric
+% registered = imregister(moving, fixed, 'similarity', optimizer, metric);
+% figure;
+% imshowpair(registered,reference);
 
 %determine transformation matrix between images
 %mytform = cp2tform(input_points,base_points,'projective');
