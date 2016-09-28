e273ee8c1ce6c6c5014e8093cfeb10f3dff649cd
Shaun Howard <smh150@case.edu>
2016-09-28 06:34:16 -0400

    got sift flow algorithm working very well.

--- project2.m
+++ project2.m
@@ -1,69 +1,51 @@
+% Register two images with SIFT and RANSAC
+% run this before use: run('VLFEATROOT/toolbox/vl_setup')
+% more directions here: http://www.vlfeat.org/install-matlab.html
+run('~/vlfeat-0.9.20/toolbox/vl_setup');
+
+% set dimensions of input images to be resized to
+dim = [256 256];
+
 % read in unregistered image
-unregistered = imread('pics/cantilever1_1.jpg');
+unregistered = imresize(imread('pics/cantilever1_2.jpg'), dim);
+% allow user to crop image
+%[unregistered,~] = imcrop(unregistered);
+%unregistered
 %figure, imshow(unregistered)
 
 % read in reference image
-reference = imread('pics/cantilever_layout1.bmp');
+reference = imresize(imread('pics/cantilever_layout1e.bmp'), dim);
+% allow user to crop image
+%[reference,~] = imcrop(reference);
+%reference
 %figure, imshow(reference)
- 
-%invoke MATLAB two image selection tool
-%[movingPoints,fixedPoints] = cpselect(unregistered(:,:,1),reference, 'Wait', true);
 
-reference = rgb2gray(reference);
-unregistered = rgb2gray(unregistered);
+im1 = rgb2gray(reference);
+size(im1)
+im2 = rgb2gray(unregistered);
+size(im2)
+patchsize = 8; % half of the window size for computing SIFT
+gridspacing = 1; % sampling step
+SiftIm1=iat_dense_sift(im2double(im1),patchsize,gridspacing);
+SiftIm2=iat_dense_sift(im2double(im2),patchsize,gridspacing); 
+figure;imshow(iat_sift2rgb(SiftIm1));title('SIFT image 1');
+figure;imshow(iat_sift2rgb(SiftIm2));title('SIFT image 2');
 
-% create RANSAC coefficient
-coeff.minPtNum = 4;
-coeff.iterNum = 30;
-coeff.thDist = 2.5;
-coeff.thInlrRatio = .5;
+SIFTflowpara.alpha=0.01;
+SIFTflowpara.d=0.2;
+SIFTflowpara.gamma=0.001;
+SIFTflowpara.nlevels=4;
+SIFTflowpara.wsize=3;
+SIFTflowpara.topwsize=10;
+SIFTflowpara.nIterations=60;
+[vx, vy, energylist] = iat_SIFTflow(SiftIm1, SiftIm2, SIFTflowpara);
 
-% order of input sometimes matters, but theoretically should not
-[transform] = sift_w_ransac(reference, unregistered, coeff);
+newIm2 = im2(patchsize/2+1:end-patchsize/2, patchsize/2+1:end-patchsize/2);
+[warpedIm2, support] = iat_pixel_warping(newIm2, vx, vy);
+newIm1 = im1(patchsize/2+1:end-patchsize/2, patchsize/2+1:end-patchsize/2);
+[~, grayerror] = iat_error2gray(newIm1,warpedIm2,support);
+figure;imshow(newIm1);title('Image1');
+figure;imshow(uint8(warpedIm2)); title('Warped Image2');
+figure;imshow(grayerror); title('Registration error'); 
 
-% ptsOriginal  = detectSURFFeatures(reference);
-% ptsDistorted = detectSURFFeatures(unregistered);
-% [featuresOriginal,validPtsOriginal] = extractFeatures(reference,ptsOriginal);
-% [featuresDistorted,validPtsDistorted] = extractFeatures(unregistered,ptsDistorted);
-% % 
-% index_pairs = matchFeatures(featuresOriginal,featuresDistorted)
-% matchedPtsOriginal  = validPtsOriginal(index_pairs(:,1));
-% matchedPtsDistorted = validPtsDistorted(index_pairs(:,2));
-% figure;
-% showMatchedFeatures(reference,unregistered,matchedPtsOriginal,matchedPtsDistorted);
-% title('Matched SURF points,including outliers');
-% 
-% [tform,inlierPtsDistorted,inlierPtsOriginal] = ...
-%     estimateGeometricTransform(matchedPtsDistorted,matchedPtsOriginal,...
-%     'similarity');
-% figure;
-% 
-% showMatchedFeatures(reference,unregistered,...
-%     inlierPtsOriginal,inlierPtsDistorted);
-% title('Matched inlier points');
-
-% % create affine transform from unregistered image to reference image
-% tform = fitgeotrans(movingPoints,fixedPoints,'projective');
-% 
-% apply generated transform on unregistered image to warp image
-%registered = imwarp(unregistered, transform, 'OutputView', imref2d(size(reference)));
-% 
-% % compute MSE between the two
-% [optimizer,metric] = imregconfig('multimodal');
-% moving = rgb2gray(warped);
-% fixed = rgb2gray(reference);
-% 
-% % attempt to register warper image with reference image using multimodal
-% % registration optimization and similarity metric
-% registered = imregister(moving, fixed, 'similarity', optimizer, metric);
-%figure;
-%imshowpair(registered,reference);
-
-%determine transformation matrix between images
-%mytform = cp2tform(input_points,base_points,'projective');
-
-%transform images to match
-%registered = imtransform(unregistered,mytform);
-
-% use inverse mapping to transform image X into image U
-% = tforminv(T,reference);
+figure;imshow(iat_error2rgb(uint8(newIm1),uint8(warpedIm2)));title('Colorized alignment error');
